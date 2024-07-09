686c3240960334914ac0e2ef7ee37e03ddff81ff
Sumit Panda <sumitpanda1308@gmail.com>
2024-07-09 15:30:48 +0530

    frontend proxy

--- uidb-frontend/src/services/api.js
+++ uidb-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000'; // Adjust this to your backend URL
+const API_URL = 'https://uidb.onrender.com'; // Adjust this to your backend URL
 
 const handleLogout = async () => {
   localStorage.removeItem('token');
